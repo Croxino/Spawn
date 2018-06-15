f24479973d80040e3d81964d3080c5e0f5d92155
Unknown <goelicheng@gmail.com>
2018-06-15 09:52:49 +0200

    Drag script

---
+++ Assets/Scripts/drag.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class drag : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+
+    public GameObject bunny;
+    private GameObject rabbitInstance;
+    public Vector3 mousePos;
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.localPosition = new Vector3(378.2f, -117.7f, 0);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    public void Spawn()
+    {
+
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            mousePos = Input.mousePosition;
+            mousePos.z = 290;
+            mousePos.x += 200;
+
+            Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+            rabbitInstance = Instantiate(bunny, objectPos, Quaternion.identity) as GameObject;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        Spawn();
+    }
+}
